aa769a04071408f6ddd844ee93b1c4fac1b19723
Naila <nailasadia@users.noreply.huggingface.co>
2025-07-05 20:09:40 +0000

    Update utils/map_utils.py

--- utils/map_utils.py
+++ utils/map_utils.py
@@ -7,7 +7,7 @@ def get_animal_habitat_map(animal_name):
         height="250"
         frameborder="0"
         style="border:0"
-        src="https://www.google.com/maps/embed/v1/search?q=habitat+of+{animal_name}&key=YOUR_GOOGLE_MAPS_API_KEY"
+        src="https://www.google.com/maps/embed/v1/search?q=habitat+of+{animal_name}&key=GOO_TOKEN"
         allowfullscreen>
     </iframe>
     """
